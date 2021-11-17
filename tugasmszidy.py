71973b63873266a5a78c6877adf54c135ccaa8c8
donnyekossa <94508137+donnyekossa@users.noreply.github.com>
2021-11-17 11:13:23 +0700

    Add files via upload

---
+++ tugasmszidy.py
@@ -0,0 +1,7 @@
+nama = 'Donny Eko Saputro'
+nim = 212410102039
+prodi = 'Teknologi Informasi'
+univ = 'Universitas Jember'
+asal = 'Blitar'
+
+print('Perkenalkan saya', nama,'dengan NIM', nim, 'dari prodi', prodi, univ, 'dan saya berasal dari', asal)
